42345747fb9140e6ac12a5019766bbf706f7daa6
Félix Dorn <github@felixdorn.fr>
2021-05-14 21:57:50 +0200

    Twitter API v2 compatibility

--- src/UserStream.php
+++ src/UserStream.php
@@ -2,29 +2,46 @@
 
 namespace Spatie\TwitterStreamingApi;
 
-use Phirehose;
+use RWC\TwitterStream\Rule;
+use RWC\TwitterStream\RuleBuilder;
+use RWC\TwitterStream\Sets;
+use RWC\TwitterStream\TwitterStream;
 
-class UserStream extends BaseStream
+class UserStream
 {
-    public function __construct(
-        string $accessToken,
-        string $accessSecret,
-        string $consumerKey,
-        string $consumerSecret
-    ) {
-        $this->stream = $this->createStream(
-            $accessToken,
-            $accessSecret,
-            $consumerKey,
-            $consumerSecret,
-            Phirehose::METHOD_USER
-        );
+    protected string $handle;
+    protected TwitterStream $stream;
+    /** @var callable */
+    protected $onTweet;
+
+    public function __construct(string $handle, string $bearerToken, string $apiKey, string $apiSecretKey)
+    {
+        $this->handle = $handle;
+        $this->stream = new TwitterStream($bearerToken, $apiKey, $apiSecretKey);
     }
 
-    public function onEvent(callable $onEvent): self
+    public static function create(string $handle, string $bearerToken, string $apiKey, string $apiSecretKey): static
     {
-        $this->stream->performOnStreamActivity($onEvent);
+        return new self($handle, $bearerToken, $apiKey, $apiSecretKey);
+    }
+
+    public function onEvent(callable $onTweet): static
+    {
+        $this->onTweet = $onTweet;
 
         return $this;
+    }
+
+    public function startListening(Sets $sets = null): void
+    {
+        // Delete old rules
+        Rule::deleteBulk(...Rule::all());
+        RuleBuilder::create()
+            ->from($this->handle)
+            ->save();
+
+        foreach ($this->stream->filteredTweets($sets) as $tweet) {
+            call_user_func($this->onTweet, $tweet);
+        }
     }
 }
